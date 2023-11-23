e84a00495e7a5a6e244db87bd95bd812130406fa
Anne Hids <anne.hids@rivm.nl>
2023-11-23 11:53:06 +0100

    Added descriptions for acceleration maneuver functions

--- R/Maneuver Acceleration.R
+++ R/Maneuver Acceleration.R
@@ -1,3 +1,25 @@
+#' Acceleration maneuver 
+#' 
+#' This function is used to calculate the distance in m and longitudinal friction work in N 
+#' of an acceleration maneuver on dry asphalt. These values are returned in a list. 
+#' 
+#' @param sector_velocity_ms Vehicle velocity at the middle of the sector (m/s)
+#' @param sector_start_velocity_ms Vehicle velocity at the start of the sector (m/s)
+#' @param frac_driver_comfort_max_accelaration Comfortable acceleration constant (unitless)
+#' @param c_accel_max Maximum acceleration 
+#' @param m_vehicle Mass of the vehicle (kg)
+#' @param m_rotate Mass of the rotating parts (kg)
+#' @param c_drag Drag coefficient of the vehicle (unitless)
+#' @param A_vehicle Vehicle frontal area (m^2)
+#' @param rho_air Density of air (kg/m^3) 
+#' @param v_wind Wind velocity (m/s)
+#' @param c_roll Roll coefficient of the tyre (kg/kg)
+#' @param grav_constant Gravitational constant (m/s^2)
+#' @param sector_alpha_slope Slope of the sector (%)
+#' @param optimal_slip_dry_asphalt Optimal Slip ratio between tyre and track (unitless)
+#' @param x_slip_long_force_dry_asphalt Longitudinal slip force on dry asphalt (N)
+#' @param accel_force_end_maneuver_1 Acceleration force at the end of the maneuver (N)
+
 f_acc_dry <- function(sector_velocity_ms,
                       sector_start_velocity_ms,
                       frac_driver_comfort_max_accelaration,
@@ -15,38 +37,51 @@ f_acc_dry <- function(sector_velocity_ms,
                       x_slip_long_force_dry_asphalt,
                       accel_force_end_maneuver_1 = 0){
   
+  ## Longitudinal friction work during acceleration
   
   maneuver_1 = "acceleration on dry asphalt"
+  
+  # start velocity of the sector
   v_start_acceleration_maneuver_1 = pmin(sector_velocity_ms,sector_start_velocity_ms) 
   
+  # end velocity of the sector
+  v_end_acceleration_maneuver_1 = sector_velocity_ms 
   
-  v_end_acceleration_maneuver_1 = sector_velocity_ms
+  # average velocity of the sector
   v_average_maneuver_1 = (v_start_acceleration_maneuver_1 + v_end_acceleration_maneuver_1)/2
   
+  # comfortable acceleration 
   c_accel_maneuver_1 <- frac_driver_comfort_max_accelaration * c_accel_max
   
+  # acceleration force
   accel_force_t0_maneuver_1 = f_accel_force(m_vehicle = m_vehicle, 
                                             m_rotate = m_rotate, 
-                                            c_accel = c_accel_maneuver_1)
+                                            c_accel = c_accel_maneuver_1) 
   
-  
+  # average acceleration force
   accel_force_average_maneuver_1 = (accel_force_t0_maneuver_1 + accel_force_end_maneuver_1)/2
   
+  # duration of the maneuver
   duration_maneuver_1 = f_accel_time(v_start = v_start_acceleration_maneuver_1, 
                                      v_end = v_end_acceleration_maneuver_1, 
                                      c_accel = c_accel_maneuver_1)
+  
+  # distance of the maneuver
   distance_maneuver_1 = f_accel_distance(v_start = v_start_acceleration_maneuver_1, 
                                          Accel_time = duration_maneuver_1, 
                                          c_accel = c_accel_maneuver_1)
   
+  # roll force
+  roll_force_maneuver_1 = f_roll_force(c_roll=c_roll , 
+                                       m_vehicle = m_vehicle, 
+                                       grav_constant = grav_constant) 
+  
+  # drag force
   drag_force_maneuver_1 = f_drag_force(c_drag = c_drag, 
                                        A_vehicle = A_vehicle, 
                                        rho_air = rho_air, 
                                        v_vehicle = v_average_maneuver_1, 
                                        v_wind = v_wind)
-  roll_force_maneuver_1 = f_roll_force(c_roll=c_roll , 
-                                       m_vehicle = m_vehicle, 
-                                       grav_constant = grav_constant) 
   
   # so here you implement to take 0 if the slope is going downhill for the force related to the slope. 
   # What about the reduced need to accelerate under a negative slope? This does not seem to be implemented.
@@ -54,6 +89,7 @@ f_acc_dry <- function(sector_velocity_ms,
                                                 grav_constant = grav_constant, 
                                                 alpha_slope = sector_alpha_slope))
   
+  # wheelspin
   slip_maneuver_1 = f_slip_wheelspin(optimal_slip = optimal_slip_dry_asphalt, 
                                      x_slip_long_force = x_slip_long_force_dry_asphalt, 
                                      long_force = (accel_force_average_maneuver_1+ 
@@ -61,10 +97,13 @@ f_acc_dry <- function(sector_velocity_ms,
                                                      roll_force_maneuver_1+ 
                                                      slope_force_maneuver_1)) # changed as pmin(0,slope_force_maneuver_1) is not expected and probably incorrect as it is always 0
   
+  # longitudinal friction work 
   longitude_friction_work_maneuver_1 = f_longitude_friction_work(long_force = (drag_force_maneuver_1 + roll_force_maneuver_1 + slope_force_maneuver_1 + accel_force_average_maneuver_1), slip = slip_maneuver_1, distance = distance_maneuver_1)
   
   # this implementation results in relationship between distance of the this maneuver and the required time needed to go from starting velocity to end velocity. Care should be taken to then allow the rest of the track distance to be driven at constant speed.
   # for this reason a list is reported of both
+  
+  # create a list containing the name of the maneuver, the distance of the maneuver and the longitudinal friction work
   list(maneuver=maneuver_1,distance=distance_maneuver_1, friction_work_longitude = longitude_friction_work_maneuver_1)
 }
 
