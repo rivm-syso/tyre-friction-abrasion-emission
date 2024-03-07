f6dbf2cc036f9119c6da97395fe2e5260f306ce7
Joris Meesters <meesterj@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-03-07 13:50:17 +0100

    started with exercises reading data from excel file

---
+++ R/sector_data_excel_reader.R
@@ -0,0 +1,13 @@
+## library set up ##
+library (readxl)
+library(dplyr)
+
+## Open Excel in R ##
+sector_data <- read_excel("R/exercise_excel.xlsx")
+
+## Select columns from sector.data ##
+
+First_column <- select(sector_data, c('First column'))
+Second_column <- select(sector_data, c('Second column'))
+Third_column <- First_column * Second_column
+
