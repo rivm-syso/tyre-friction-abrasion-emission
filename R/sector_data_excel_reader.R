7e4ce95d067873341dcc0dc5d3d660cfb2dde62a
Joris Meesters <meesterj@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-03-11 12:50:58 +0100

    starting with exceld read of maneuver input data

--- R/sector_data_excel_reader.R
+++ R/sector_data_excel_reader.R
@@ -3,11 +3,13 @@ library (readxl)
 library(dplyr)
 
 ## Open Excel in R ##
-sector_data <- read_excel("R/exercise_excel.xlsx")
+maneuver_data <- read_excel("R/exercise_excel_2.xlsx")
 
 ## Select columns from sector_data ##
 
-First_column <- select(sector_data, c('First column'))
-Second_column <- select(sector_data, c('Second column'))
-Third_column <- First_column * Second_column
+
+
+#First_column <- select(sector_data, c('First column'))
+#Second_column <- select(sector_data, c('Second column'))
+#Third_column <- First_column * Second_column
 
