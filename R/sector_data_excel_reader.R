14cb6667759ed06074ed65b428a0cc3e1c7e39a1
Joris Meesters <meesterj@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-03-12 10:11:14 +0100

    excel data reading

--- R/sector_data_excel_reader.R
+++ R/sector_data_excel_reader.R
@@ -7,6 +7,19 @@ maneuver_data <- read_excel("R/exercise_excel_2.xlsx")
 
 ## Select columns from sector_data ##
 
+v_start <- select(maneuver_data, c ('Start speed')) 
+v_end <- select (maneuver_data, c ('End speed'))
+v_top <- select (maneuver_data, c ('Top speed'))
+alpha_slope <- atan(select(maneuver_data, c('Slope')))
+corner_angle <- select(maneuver_data, c ('Corner angle'))
+r_corner <- select(maneuver_data, c ('Corner radius')) 
+c_accel <- select(maneuver_data, c ('c_accel'))
+c_decel <- select(maneuver_data, c ('c_decel'))
+maneuver_repeats <- select(maneuver_data, c ('Maneuver repeats'))
+
+
+
+
 
 
 #First_column <- select(sector_data, c('First column'))
