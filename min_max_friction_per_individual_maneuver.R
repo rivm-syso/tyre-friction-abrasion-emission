243398dd7d45e4ec520a50bc18bb1c2d5297eb78
Joris Meesters <meesterj@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-05-29 11:52:41 +0200

    minima and maxima friction work intensities

--- min_max_friction_per_individual_maneuver.R
+++ min_max_friction_per_individual_maneuver.R
@@ -23,5 +23,29 @@ AllData_fw <- AllData_fw |> mutate("Min FW Accellong per m" = min(FWAccellong_pe
 
 const_speed_20_kph_data <- AllData_fw |> filter(`Maneuver number`== "MUcs20kph1")
 min_FW_per_m_const_speed_20_kph = min(const_speed_20_kph_data$`Min FW Const speed per m`)
-max_FW_per_m_const_speed_20_kph = max(const_speed_20_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_20_kph = max(const_speed_20_kph_data$`Max FW Const speed per m`)
+
+const_speed_30_kph_data <- AllData_fw |> filter(`Maneuver number`== "MUpa30kph1")
+min_FW_per_m_const_speed_30_kph = min(const_speed_30_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_30_kph = max(const_speed_30_kph_data$`Max FW Const speed per m`)
+
+const_speed_40_kph_data <- AllData_fw |> filter(`Maneuver number`== "MUpa40kph1")
+min_FW_per_m_const_speed_40_kph = min(const_speed_40_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_40_kph = max(const_speed_40_kph_data$`Max FW Const speed per m`)
+
+const_speed_60_kph_data <- AllData_fw |> filter(`Maneuver number`== "MRu60kph3")
+min_FW_per_m_const_speed_60_kph = min(const_speed_60_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_60_kph = max(const_speed_60_kph_data$`Max FW Const speed per m`)
+
+const_speed_70_kph_data <- AllData_fw |> filter(`Maneuver number`== "MRu70kph3")
+min_FW_per_m_const_speed_70_kph = min(const_speed_70_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_70_kph = max(const_speed_70_kph_data$`Max FW Const speed per m`)
+
+const_speed_80_kph_data <- AllData_fw |> filter(`Maneuver number`== "MRu70kph3")
+min_FW_per_m_const_speed_80_kph = min(const_speed_80_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_80_kph = max(const_speed_80_kph_data$`Max FW Const speed per m`)
+
+const_speed_130_kph_data <- AllData_fw |> filter(`Maneuver number`== "MMoW3")
+min_FW_per_m_const_speed_130_kph = min(const_speed_130_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_130_kph = max(const_speed_130_kph_data$`Max FW Const speed per m`)
 
