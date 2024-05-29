209201b618211a97a8ecfcb39c47926fe3aa6804
Joris Meesters <meesterj@rivm-rsrv-l01p.rivm.ssc-campus.nl>
2024-05-29 10:57:04 +0200

    filter minima and maxima J per m from alldata_fw

--- min_max_friction_per_individual_maneuver.R
+++ min_max_friction_per_individual_maneuver.R
@@ -1,3 +1,27 @@
 library(dplyr)
+
+# mutate AllData_fw to get friction work intensity in J per m
+
+AllData_fw<- AllData_fw |> mutate ("FWAccellong_per_m" = list(FWAccelLong/DistanceManAccel)
+                                          , "FWDecellong_per_m" =list(FWDecelLong/DistanceManDecel)
+                                          ,"FWConstSpeed_per_m" = list(FWConstLong/DistanceConst))
+
+
+# add minima and maxima to AllData_fw
+
+AllData_fw <- AllData_fw |> mutate("Min FW Accellong per m" = min(FWAccellong_per_m)
+                                  ,"Max FW Accellong per m" = max(FWAccellong_per_m),
+                                  "Min FW Decellong per m" = min(FWDecellong_per_m)
+                                  , "Max FW Decellong per m" = max(FWDecellong_per_m)
+                                  , "Min FW Const speed per m" = min(FWConstSpeed_per_m)
+                                  , "Max FW Const speed per m" = max(FWConstSpeed_per_m))
+
+
+
+
+# filter Alldata_fw_per_m for different maneuvers and get minimum and maximum friction work intensity per maneuver
+
 const_speed_20_kph_data <- AllData_fw |> filter(`Maneuver number`== "MUcs20kph1")
-const_speed_20_kph_data <- const_speed_20_kph_data |> mutate("FW per m" = list(FWConstLong / DistanceConst))
+min_FW_per_m_const_speed_20_kph = min(const_speed_20_kph_data$`Min FW Const speed per m`)
+max_FW_per_m_const_speed_20_kph = max(const_speed_20_kph_data$`Min FW Const speed per m`)
+
